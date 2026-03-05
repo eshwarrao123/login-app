3a32de0a9bed255d265e8c8583639629151b10c1
Eshwar Rao <147790101+eshwarrao123@users.noreply.github.com>
2026-03-05 14:38:34 +0530

    Update Login.js

--- frontend/src/Login.js
+++ frontend/src/Login.js
@@ -9,7 +9,7 @@ const Login = () => {
     const handleLogin = async (e) => {
         e.preventDefault();
         try {
-            const response = await axios.post('https://login-app-backend-0yzd.onrender.com/', { username, password });
+            const response = await axios.post('https://login-app-backend-0yzd.onrender.com/login', { username, password });
             if (response.status === 200) {
                 localStorage.setItem('username', username);
                 window.location.href = '/welcome';
